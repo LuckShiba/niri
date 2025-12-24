de8727be0462b3c2d9555b73e956b668a7ce6873
LuckShiba <luckshiba@protonmail.com>
2025-12-24 17:37:52 -0300

    actually tests were broken

--- flake.nix
+++ flake.nix
@@ -137,6 +137,7 @@
           cargoTestOptions = old: old ++ [
             # These tests require the ability to access a "valid EGL Display", but that won't work
             # inside the Nix sandbox
+            "--"
             "--skip=::egl"
           ];
 
